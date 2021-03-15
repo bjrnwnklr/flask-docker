e3ad307d2bf791978c339d062737f38eaf3a993f
Bjoern Winkler <bjrnwnklr@gmail.com>
2021-03-15 22:04:54 +0100

    updated setup.py with author name

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='flaskexampleapp',
-    packages=['flaskexampleapp'],
+    author='Bjoern Winkler',
+    description='A flask / docker example app',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
